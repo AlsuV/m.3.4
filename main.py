0b01e59f9f3b586c39e2b95619a8c3883c21efc4
AlsuV <165937069+AlsuV@users.noreply.github.com>
2024-08-16 20:08:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def single_root_words(root_word, *other_words):
         if root_word.lower() in word.lower() or word.upper() in root_word.upper():
             same_words.append(word.lower())
 
-        return same_words
+    return same_words
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
